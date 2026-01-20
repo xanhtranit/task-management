702d6da37156be45e0e2bb33f31ccd3af1c0fdaf
xanhtranit <237573681+xanhtranit@users.noreply.github.com>
2026-01-20 17:05:56 +0700

    feat: export tasks and user-task data to Excel file

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const app = express();
 const authRoutes = require("./routes/authRoutes")
 const userRoutes = require("./routes/userRoutes")
 const taskRoutes = require("./routes/taskRoutes")
+const reportRoutes = require("./routes/reportRoutes")
 
 //Middleware to handle CORS
 app.use(
@@ -26,7 +27,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
-// app.use("/api/reports", reportRoutes);
+app.use("/api/reports", reportRoutes);
 
 //Start serve
 const PORT = process.env.PORT || 5000;
